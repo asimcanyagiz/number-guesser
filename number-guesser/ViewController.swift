487f3557c7d86902c95b512a9ea7178d3b053e31
Asim Can Yagiz <meycasim@gmail.com>
2022-08-09 14:10:58 +0300

    View Controller Elements Builded

--- number-guesser/ViewController.swift
+++ number-guesser/ViewController.swift
@@ -8,11 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var txtNumber: UITextField!
+    @IBOutlet weak var imgNumber: UIImageView!
+    @IBOutlet weak var btnNumber: UIButton!
+    
+    
+    @IBOutlet weak var txtGuess: UITextField!
+    @IBOutlet weak var imgGuess: UIImageView!
+    @IBOutlet weak var btnGuess: UIButton!
+    
+    
+    @IBOutlet weak var star1: UIImageView!
+    @IBOutlet weak var star2: UIImageView!
+    @IBOutlet weak var star3: UIImageView!
+    @IBOutlet weak var star4: UIImageView!
+    @IBOutlet weak var star5: UIImageView!
+    
+    
+    @IBOutlet weak var lblResults: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func btnNumberClicked(_ sender: UIButton) {
+    }
+    @IBAction func btnGuessClicked(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
